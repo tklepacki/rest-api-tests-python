0f74a622c4d5c910d3407a9dc9727d644f7d39a3
Tomasz Klepacki <t.klepacki@wp.pl>
2026-05-07 00:20:04 +0200

    Adding header

--- services/part_01/users.py
+++ services/part_01/users.py
@@ -4,7 +4,7 @@ def get_user(id):
     url = f"https://reqres.in/api/users/{id}"
 
     session = requests.session()
-    response = session.get(url)
+    response = session.get(url, headers={'x-api-key': 'reqres_eb68f411937a40c68226fb013febfe14'})
 
     return response
 
@@ -12,6 +12,6 @@ def get_users_list(page_id):
     url = "https://reqres.in/api/users"
 
     session = requests.session()
-    response = session.get(url, params={'page': page_id})
+    response = session.get(url, params={'page': page_id}, headers={'x-api-key': 'reqres_eb68f411937a40c68226fb013febfe14'})
 
     return response
